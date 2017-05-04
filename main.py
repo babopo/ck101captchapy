912b8b1b745f2594b35c5d62720e79a893c903d6
limbo <llb123@live.com>
2017-05-04 10:28:34 +0800

    classifier module

--- main.py
+++ main.py
@@ -8,28 +8,7 @@ import cv2
 from scipy import misc
 from skimage import morphology
 from matplotlib import pyplot as plt
-
-
-def normal(I):  # 将字符图片归一化至28*28，邻近插值，返回处理后的数组
-    w_n = 28
-    h_n = 28
-    m, n = I.shape
-    w = w_n / n
-    h = h_n / m
-    imgn = np.zeros([28, 28]).astype(int)
-    rot = np.array([[h, 0, 0], [0, w, 0], [0, 0, 1]])  # 变换矩阵 x = h*u, y = w*v
-    rotI = np.mat(rot).I  # rot矩阵求逆
-    for row in range(h_n):
-        for col in range(w_n):
-            pix = np.mat([row, col, 1])
-            pix = pix * rotI
-            pix = np.array(pix).astype(int)  # 矩阵索引不太方便，转为数组
-            if pix[0][0] < 1:
-                pix[0][0] = 1
-            if pix[0][1] < 1:
-                pix[0][1] = 1
-            imgn[row][col] = I[round(pix[0][0])][round(pix[0][1])]
-    return imgn
+import normal
 
 
 def filename(file):  # 读取目录下特定顺序文件
@@ -95,7 +74,7 @@ for i in range(K):
                 for col in range(w):
                     cut[row][col] = LAB_BW[high[0][0] + row][width[0][0] + col]
             pos = width[0][0] / 120
-            cut_n = normal(cut).astype(float)  # 转为float类型以便opencv显示
+            cut_n = normal.normal(cut).astype(float)  # 转为float类型以便opencv显示
             cv2.imshow(windowname, cut_n)
             savename = ".\\temp\\" + str(pos) + ".png"  # 相对路径，方便修改
             misc.imsave(savename, cut_n)  # 将array保存为图像
@@ -111,12 +90,6 @@ for i in range(0, len(os.listdir(".\\temp\\"))):
             if I[row][col] == 0:
                 numB[rowB][colB] = numB[rowB][colB] + 1
     fea = numB.reshape(1, 16)  # 16位向量作为特征储存
-
-
-
-
-
-
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
