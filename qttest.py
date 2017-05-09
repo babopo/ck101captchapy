bf744aca77c94a0b724aebf4e70064844cd66693
limbo <llb123@live.com>
2017-05-09 11:39:31 +0800

    GUI rebuilt

--- qttest.py
+++ qttest.py
@@ -7,13 +7,13 @@
 # sys.exit(app.exec_())
 
 import sys
-import test
+import CAPTCHA
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
-    ui = test.Ui_MainWindow()
+    ui = CAPTCHA.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
